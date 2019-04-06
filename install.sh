2e9455adbcfafa292e034ded231e3a2ad286dc57
Stijn De Haes <stijndehaes@gmail.com>
2019-04-06 22:23:10 +0200

    switch to helm chart instead of chart on local path

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ helm init
 
 kubectl apply -f secret.yaml
 kubectl apply -f configmap.yaml
-helm upgrade --install airflow ../charts/stable/airflow -f values.yaml --version 2.4.3
+helm upgrade --install airflow stable/airflow -f values.yaml --version 2.4.4
